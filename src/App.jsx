ab5afc74751ebb96d835c8e896d6d8275ecfe2a7
Madhumitha <madhumithavenu03@gmail.com>
2023-11-11 11:05:11 +0530

    created components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import{Chip, Divider} from '@mui/material'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Divider><Chip label="Calender"/></Divider>
+      <CalenderComp />
+    </>
+  );
+}
+
+export default App;
