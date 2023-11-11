b4d5a79f6682eb19f8c05771c7b2ddff9f5fca0a
Madhumitha <madhumithavenu03@gmail.com>
2023-11-11 12:57:21 +0530

    function to escape button

--- src/components/DateRangeComp.jsx
+++ src/components/DateRangeComp.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState, useRef} from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+// import { DateRange } from 'react-date-range';
+// import { TextField } from '@mui/material';
+import { addDays } from 'date-fns';
 
 function DateRangeComp() {
+
+  const [range, setRange] = useState([{
+    startDate: new Date(),
+    endDate: addDays(new Date(), 7),
+    key: 'selection'
+  }]);
+
+  const [open, setOpen] = useState(false);
+  const refOne = useRef(null);
+
+  function hideOnEscape(e) {
+    if(e.key === 'Escape'){
+      setOpen(false);
+    }
+  }
+  function hideOnClickOutside(e) {
+    if(refOne.current && !refOne.current.contains(e.target)){
+      setOpen(false);
+    }
+  }
+
+  useEffect(()=>{
+    document.addEventListener("keydown", hideOnEscape, true);
+    document.addEventListener("click", hideOnClickOutside, true);
+  },[]);
+
   return (
     <div>DateRangeComp</div>
   )
